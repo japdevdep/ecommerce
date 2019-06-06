d20cd18aa405db6b5f9d69b9006d679f54578591
Sebastián Caggiano <seba2610@gmail.com>
2019-06-06 16:53:40 -0300

    Se commitea todo lo hecho hasta ahora, se ramificará para elaborar uno completo.

--- js/products.js
+++ js/products.js
@@ -29,7 +29,7 @@ function showProductsList(array){
         let product = array[i];
 
         htmlContentToAppend += `
-        <a href="category-info.html" class="list-group-item list-group-item-action">
+        <a href="product-info.html" class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
                     <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
